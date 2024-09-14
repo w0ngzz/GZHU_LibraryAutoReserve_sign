774ed459e3ac9db9c965ee8971e8c0c946301db6
wongzz <34603964+w0ngzz@users.noreply.github.com>
2024-09-14 12:53:43 +0800

    Update info.py

--- libs/info.py
+++ libs/info.py
@@ -3,11 +3,12 @@ import os
 # 获取特定环境变量的值
 sno = os.environ.get('SNO')
 pwd = os.environ.get('PWD')
+seat = os.environ.get('SEAT')
 infos = [
     {
         'sno': sno,         # 学号
         'pwd': pwd,         # 密码
-        'devName': '204-017',   # 预约的座位号（不足3位数的要补零）
+        'devName': seat,   # 预约的座位号（不足3位数的要补零）
         'name': '猪猪侠',        # 随便起个名字
         'periods': (            # 预约时间段（每段时间不能超过4小时）
             ('8:30:00', '13:30:00'),
